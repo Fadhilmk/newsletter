7a168eae898c88bd997eb48f7d371bac87d3358b
Fadhil <boggaming098.com@gmail.com>
2024-06-04 15:26:53 +0530

    hu

--- app/dashboard/subscribers-form/page.jsx
+++ app/dashboard/subscribers-form/page.jsx
@@ -1,38 +1,73 @@
 // app/dashboard/user/page.jsx
+// import React from "react";
+// import "./styles.css";
+
+// const SubscribersFormPage = () => {
+
+
+//   return (
+//     <div>
+//       <h1 className="heading">Subscribers Form</h1>
+//     <div className="container">
+//       <div className="left-side">
+//         <pre className="pre-style">
+//           {`
+//           <iframe src="/subscribe-form" data-test-id="subscribe-form" width="100%" height="320" 
+//           frameBorder="0" scrolling="no" style={{ borderRadius: "4px", 
+//           border: "2px solid #e5e7eb", margin: "0", backgroundColor: "transparent",}}></iframe>
+//           `}
+//         </pre>
+//       </div>
+
+//       <div className="right-side">
+//         <iframe
+//           src=""
+//           data-test-id=""
+//           width="100%"
+//           height="320"
+//           frameBorder=""
+//           scrolling="no"
+//           className="iframe-style"
+//         ></iframe>
+//       </div>
+//     </div>
+//     </div>
+//   );
+// }; 
+
+// export default SubscribersFormPage;
+// app/dashboard/user/page.jsx
 import React from "react";
 import "./styles.css";
 
 const SubscribersFormPage = () => {
-
-
   return (
     <div>
       <h1 className="heading">Subscribers Form</h1>
-    <div className="container">
-      <div className="left-side">
-        <pre className="pre-style">
-          {`
-          <iframe src="/subscribe-form" data-test-id="subscribe-form" width="100%" height="320" 
-          frameBorder="0" scrolling="no" style={{ borderRadius: "4px", 
-          border: "2px solid #e5e7eb", margin: "0", backgroundColor: "transparent",}}></iframe>
-          `}
-        </pre>
-      </div>
+      <div className="container">
+        <div className="left-side">
+          <pre className="pre-style">
+            {`
+<iframe src="/form.html" data-test-id="subscribe-form" width="100%" height="320" 
+frameBorder="0" scrolling="no" style="borderRadius: 4px; border: 2px solid #e5e7eb; margin: 0; backgroundColor: transparent;"></iframe>
+            `}
+          </pre>
+        </div>
 
-      <div className="right-side">
-        <iframe
-          src=""
-          data-test-id=""
-          width="100%"
-          height="320"
-          frameBorder=""
-          scrolling="no"
-          className="iframe-style"
-        ></iframe>
+        <div className="right-side">
+          <iframe
+            src="/form.html"
+            data-test-id="subscribe-form"
+            width="100%"
+            height="320"
+            frameBorder="0"
+            scrolling="no"
+            className="iframe-style"
+          ></iframe>
+        </div>
       </div>
-    </div>
     </div>
   );
-}; 
+};
 
 export default SubscribersFormPage;
